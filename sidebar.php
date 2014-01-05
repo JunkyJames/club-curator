0b1cfbc8534b1c1c3250ab16283b715af66e2e52
James Cooper-Stanbury <thatguy@jpcs.me>
2014-01-05 16:43:15 +1100

    make /register show login page with registration form

--- sidebar.php
+++ sidebar.php
@@ -15,10 +15,10 @@
             <!-- Main navigation. Refer Notes.txt files for reference. -->
             <!-- Use the class "current" in main menu to hightlight current main menu -->
             <li class="current"><a href="/"><i class="icon-home"></i> Home</a></li>
-            <li><a href="contact"><i class="icon-phone"></i> Contact</a></li>
-            <li><a href="pricing"><i class="icon-dollar"></i> Pricing</a></li>
-            <li><a href="pricing"><i class="icon-dashboard"></i> Login</a></li>
-            <li><a href="pricing"><i class="icon-user"></i> Register</a></li>
+            <li><a href="/contact"><i class="icon-phone"></i> Contact</a></li>
+            <li><a href="/pricing"><i class="icon-dollar"></i> Pricing</a></li>
+            <li><a href="/login"><i class="icon-dashboard"></i> Login</a></li>
+            <li><a href="/login#register"><i class="icon-user"></i> Register</a></li>
         </ul>               
     </div>
 </div>
